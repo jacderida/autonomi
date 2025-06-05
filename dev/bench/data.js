263a606c516987e70a0ffa9f34c894395ebf732e
github-action-benchmark <github@users.noreply.github.com>
2025-06-05 12:17:33 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for b5dbfa4568e56b84035f1a1c0889722a36ffa38e

---
+++ dev/bench/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1749125853082,
+  "repoUrl": "https://github.com/jacderida/autonomi",
+  "entries": {
+    "`safe files` benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "36204420+vphongph@users.noreply.github.com",
+            "name": "vphongph",
+            "username": "vphongph"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b5dbfa4568e56b84035f1a1c0889722a36ffa38e",
+          "message": "Merge pull request #2991 from mickvandijke/example-network-from-code\n\nchore: add a network spawner example",
+          "timestamp": "2025-06-05T06:57:54Z",
+          "tree_id": "1ce560c1ae4792eecbd57b02101e7a0a471e1a6d",
+          "url": "https://github.com/jacderida/autonomi/commit/b5dbfa4568e56b84035f1a1c0889722a36ffa38e"
+        },
+        "date": 1749125852376,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 0.26196698962393844,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 1.2941911985931638,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 32.551795859988395,
+            "unit": "MiB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
