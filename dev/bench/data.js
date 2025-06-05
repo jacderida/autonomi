9a3f9fcec3b459a86c276f01b16f085d780812a9
github-action-benchmark <github@users.noreply.github.com>
2025-06-05 18:38:51 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 1c8a2c2439404bfdd2c0011092e025bb9ce76382

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749148730174,
+  "lastUpdate": 1749148731219,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -163,6 +163,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/jacderida/autonomi/commit/b5dbfa4568e56b84035f1a1c0889722a36ffa38e"
         },
         "date": 1749125990427,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 14,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 13,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mickvd99@gmail.com",
+            "name": "Mick van Dijke",
+            "username": "mickvandijke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1c8a2c2439404bfdd2c0011092e025bb9ce76382",
+          "message": "Merge pull request #2739 from mickvandijke/example-spawn-single-node\n\nchore: add spawn single node example",
+          "timestamp": "2025-06-05T14:33:07Z",
+          "tree_id": "f2200f6de6d00c507ab904118207df4887cfb39c",
+          "url": "https://github.com/jacderida/autonomi/commit/1c8a2c2439404bfdd2c0011092e025bb9ce76382"
+        },
+        "date": 1749148730949,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
