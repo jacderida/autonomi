79608e180e52d731521390b6defba30ad599336f
Roland Sherwin <RolandSherwin@protonmail.com>
2025-08-01 07:36:46 +0530

    fix(node): add external address without any filtering

--- ant-node/src/networking/mod.rs
+++ ant-node/src/networking/mod.rs
@@ -87,24 +87,6 @@ pub fn sort_peers_by_key<T>(
 #[derive(Clone, Debug, Default)]
 pub struct Addresses(pub Vec<Multiaddr>);
 
-/// Verifies if `Multiaddr` contains IPv4 address that is not global.
-/// This is used to filter out unroutable addresses from the Kademlia routing table.
-pub(crate) fn multiaddr_is_global(multiaddr: &Multiaddr) -> bool {
-    !multiaddr.iter().any(|addr| match addr {
-        Protocol::Ip4(ip) => {
-            // Based on the nightly `is_global` method (`Ipv4Addrs::is_global`), only using what is available in stable.
-            // Missing `is_shared`, `is_benchmarking` and `is_reserved`.
-            ip.is_unspecified()
-                | ip.is_private()
-                | ip.is_loopback()
-                | ip.is_link_local()
-                | ip.is_documentation()
-                | ip.is_broadcast()
-        }
-        _ => false,
-    })
-}
-
 /// Craft valid multiaddr like /ip4/68.183.39.80/udp/31055/quic-v1
 /// RelayManager::craft_relay_address for relayed addr. This is for non-relayed addr.
 pub(crate) fn craft_valid_multiaddr_without_p2p(addr: &Multiaddr) -> Option<Multiaddr> {
