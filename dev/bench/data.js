8e45f4bb11ff073250d7dd04b504f53405657530
github-action-benchmark <github@users.noreply.github.com>
2025-08-20 19:29:11 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 3a38745fb78945477140db25305809fc0cd67188

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755718072942,
+  "lastUpdate": 1755718151373,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -307,6 +307,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/jacderida/autonomi/commit/39eb71ffd9b5b8938721e3a0a5f4146d22dda565"
         },
         "date": 1755208684148,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 88,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3a38745fb78945477140db25305809fc0cd67188",
+          "message": "Merge pull request #3164 from RolandSherwin/ci_race_fix\n\nfix(node): add and sync on the same thread",
+          "timestamp": "2025-08-19T12:18:28Z",
+          "tree_id": "be8a99ba7a83c9a524026960776abb59f806ca73",
+          "url": "https://github.com/jacderida/autonomi/commit/3a38745fb78945477140db25305809fc0cd67188"
+        },
+        "date": 1755718150695,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
