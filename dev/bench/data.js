9555ae152401ca80414de2e96128a3c56e3d4435
github-action-benchmark <github@users.noreply.github.com>
2025-09-02 13:15:28 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 90bf48b1473a02fef6d690fddab5d27455d542d6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756818847878,
+  "lastUpdate": 1756818928621,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -647,6 +647,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/jacderida/autonomi/commit/ff0556631d6e76e062e9b540bcbf8e394f55dbec"
         },
         "date": 1756419097970,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 90,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "90bf48b1473a02fef6d690fddab5d27455d542d6",
+          "message": "Merge pull request #3160 from vphongph/docs-update-examples\n\ndocs: added scratchpad examples from docs and removed outdated examples",
+          "timestamp": "2025-09-02T11:00:07Z",
+          "tree_id": "9a0c838d9387787e7c0507da16c3550f143c605c",
+          "url": "https://github.com/jacderida/autonomi/commit/90bf48b1473a02fef6d690fddab5d27455d542d6"
+        },
+        "date": 1756818927909,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
