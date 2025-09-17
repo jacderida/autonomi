4049a3f53d50f6be3af0fb171b80cc3a439d79e0
github-action-benchmark <github@users.noreply.github.com>
2025-09-17 14:22:32 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 14a0f6018105c8acb75b3b7a22f9c46b0590c44a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758118877505,
+  "lastUpdate": 1758118952822,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -720,6 +720,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 90,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anselmega@gmail.com",
+            "name": "Anselme",
+            "username": "grumbach"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "14a0f6018105c8acb75b3b7a22f9c46b0590c44a",
+          "message": "Merge pull request #3202 from grumbach/various_api_cleanup\n\nchore: cleanup api",
+          "timestamp": "2025-09-16T10:37:18Z",
+          "tree_id": "f9930ab71f036fefacf0e9376fffbb7550cba0a2",
+          "url": "https://github.com/jacderida/autonomi/commit/14a0f6018105c8acb75b3b7a22f9c46b0590c44a"
+        },
+        "date": 1758118952088,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 91,
             "unit": "MB"
           }
         ]
