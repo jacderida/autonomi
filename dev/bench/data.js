632a8405c418db9a14a61ee8b53ddc452a10e95e
github-action-benchmark <github@users.noreply.github.com>
2025-11-12 16:25:58 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 01aef5794ab4d33560bc37c8886befb9a4f79a35

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762964756259,
+  "lastUpdate": 1762964758164,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -1693,6 +1693,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/jacderida/autonomi/commit/5e42bfe0fe8972f519eb1762c80f613d3042aeae"
         },
         "date": 1762301715096,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "01aef5794ab4d33560bc37c8886befb9a4f79a35",
+          "message": "Merge pull request #3296 from RolandSherwin/recursive_analyze_public\n\nfix(ant): obtain the correct public archive addresses",
+          "timestamp": "2025-11-08T12:37:50Z",
+          "tree_id": "5f218c9fb2a81d86e8426aaf585799304afd09e3",
+          "url": "https://github.com/jacderida/autonomi/commit/01aef5794ab4d33560bc37c8886befb9a4f79a35"
+        },
+        "date": 1762964757415,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
