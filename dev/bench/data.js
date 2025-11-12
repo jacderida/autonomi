413a726d5f69d70f67c87aabd50e8b0afde2c1dd
github-action-benchmark <github@users.noreply.github.com>
2025-11-12 16:25:56 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 01aef5794ab4d33560bc37c8886befb9a4f79a35

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762964675535,
+  "lastUpdate": 1762964756259,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -1128,6 +1128,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 195,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "01aef5794ab4d33560bc37c8886befb9a4f79a35",
+          "message": "Merge pull request #3296 from RolandSherwin/recursive_analyze_public\n\nfix(ant): obtain the correct public archive addresses",
+          "timestamp": "2025-11-08T12:37:50Z",
+          "tree_id": "5f218c9fb2a81d86e8426aaf585799304afd09e3",
+          "url": "https://github.com/jacderida/autonomi/commit/01aef5794ab4d33560bc37c8886befb9a4f79a35"
+        },
+        "date": 1762964755561,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 183,
             "unit": "MB"
           }
         ]
