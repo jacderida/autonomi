153a720fb4016105a26ccc3a4f1ca2b8a7b25b96
github-action-benchmark <github@users.noreply.github.com>
2025-11-13 15:22:03 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 66ec46ce23645a5e6c76f0005044de2a3b848297

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763047322330,
+  "lastUpdate": 1763047323444,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -1897,6 +1897,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/jacderida/autonomi/commit/cf69006c64e534730ddab11cd5eaa0c7942c647c"
         },
         "date": 1762988348373,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "66ec46ce23645a5e6c76f0005044de2a3b848297",
+          "message": "Merge pull request #3299 from maqi/blind_scan\n\nfurther storage tracking facilities from the analyze tool",
+          "timestamp": "2025-11-13T14:56:01Z",
+          "tree_id": "f5d149922289ce9d088f4182af801f233e18e152",
+          "url": "https://github.com/jacderida/autonomi/commit/66ec46ce23645a5e6c76f0005044de2a3b848297"
+        },
+        "date": 1763047323152,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
