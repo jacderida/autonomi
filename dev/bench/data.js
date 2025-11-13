6e652e793656b6f98c888567a9ec16c6d097d592
github-action-benchmark <github@users.noreply.github.com>
2025-11-13 15:22:04 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 66ec46ce23645a5e6c76f0005044de2a3b848297

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763047323444,
+  "lastUpdate": 1763047324614,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -2640,6 +2640,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 19186,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 1,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "66ec46ce23645a5e6c76f0005044de2a3b848297",
+          "message": "Merge pull request #3299 from maqi/blind_scan\n\nfurther storage tracking facilities from the analyze tool",
+          "timestamp": "2025-11-13T14:56:01Z",
+          "tree_id": "f5d149922289ce9d088f4182af801f233e18e152",
+          "url": "https://github.com/jacderida/autonomi/commit/66ec46ce23645a5e6c76f0005044de2a3b848297"
+        },
+        "date": 1763047324323,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 11601,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 21381,
             "unit": "ms"
           },
           {
