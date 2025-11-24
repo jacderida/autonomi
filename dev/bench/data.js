2222059fa28a8cbe14372ddcca9a6fc664b007d4
github-action-benchmark <github@users.noreply.github.com>
2025-11-24 17:51:37 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for b762a2e7aa97a7ba1a6c0bb637937578aa30377b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763768061265,
+  "lastUpdate": 1764006697084,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -779,6 +779,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "ant files download",
             "value": 6.755856165478899,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b762a2e7aa97a7ba1a6c0bb637937578aa30377b",
+          "message": "Merge pull request #3316 from jacderida/chore-new_clippy_fixes\n\nchore: upgrade `assert_cmd` reference",
+          "timestamp": "2025-11-24T09:47:13Z",
+          "tree_id": "15379df4172a3270c7e0cfb4b6fa6eb14d765245",
+          "url": "https://github.com/jacderida/autonomi/commit/b762a2e7aa97a7ba1a6c0bb637937578aa30377b"
+        },
+        "date": 1764006695618,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 1.1143677773922667,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 6.357923671709903,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 7.022136236677738,
             "unit": "MiB/s"
           }
         ]
