0e1da9f6ab15297830a83ca940ea184887a6774c
github-action-benchmark <github@users.noreply.github.com>
2025-11-27 00:35:51 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 48bce6fe350208dc66245d5ce5f1924362944424

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764203749711,
+  "lastUpdate": 1764203751592,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -3204,6 +3204,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 17622,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 1,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "48bce6fe350208dc66245d5ce5f1924362944424",
+          "message": "Merge pull request #3314 from maidsafe/dependabot/github_actions/actions/checkout-6\n\nchore(deps): bump actions/checkout from 5 to 6",
+          "timestamp": "2025-11-26T12:11:07Z",
+          "tree_id": "82e585d9c7c20ccd7c772ecc9869ea072e3c3cd1",
+          "url": "https://github.com/jacderida/autonomi/commit/48bce6fe350208dc66245d5ce5f1924362944424"
+        },
+        "date": 1764203750856,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 9850,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 18036,
             "unit": "ms"
           },
           {
