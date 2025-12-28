aae6a9032d4aa98a02013b90bfd2deeade4ec392
github-action-benchmark <github@users.noreply.github.com>
2025-12-28 17:33:31 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for d7fe852c8660e8560a40062bf7b3e14e9cce5d35

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766943210628,
+  "lastUpdate": 1766943211894,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -4050,6 +4050,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 20701,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d7fe852c8660e8560a40062bf7b3e14e9cce5d35",
+          "message": "Merge pull request #3384 from maqi/mutable_data_fall_back_DM_fetch\n\nfeat(client): using req/rsp DM as a fallback during mutable_data fetch",
+          "timestamp": "2025-12-23T23:27:20Z",
+          "tree_id": "2200bf0658f414052964b6eaebebc5095fd6aaed",
+          "url": "https://github.com/jacderida/autonomi/commit/d7fe852c8660e8560a40062bf7b3e14e9cce5d35"
+        },
+        "date": 1766943211536,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 10467,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 21350,
             "unit": "ms"
           },
           {
