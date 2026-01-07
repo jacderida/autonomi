6aa960154ed8367c7872eee84df43ba0312d8cb6
github-action-benchmark <github@users.noreply.github.com>
2026-01-07 14:26:56 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 1a4f46658ee2ec941e21d68baa6a90fe6e4318c4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767796015147,
+  "lastUpdate": 1767796016706,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -3121,6 +3121,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/jacderida/autonomi/commit/2822430d8c752a7684fb2700745917d38b7f9563"
         },
         "date": 1767711913388,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1a4f46658ee2ec941e21d68baa6a90fe6e4318c4",
+          "message": "Merge pull request #3376 from mickvandijke/feat-analytical-tools\n\nfeat: add developer query `closest-peers`",
+          "timestamp": "2026-01-07T13:10:23Z",
+          "tree_id": "a378e2fe33a70f13be8d887cc3d47c7a4f950e9b",
+          "url": "https://github.com/jacderida/autonomi/commit/1a4f46658ee2ec941e21d68baa6a90fe6e4318c4"
+        },
+        "date": 1767796016293,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
