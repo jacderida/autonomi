21b1535603d1380ab690949f43ef37a1a5e2bccd
github-action-benchmark <github@users.noreply.github.com>
2026-02-10 22:27:05 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for b9b46a353a20223b76b690294135f66b4a96849e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770660054452,
+  "lastUpdate": 1770762425857,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -1442,6 +1442,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "ant files download",
             "value": 6.577669206560919,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b9b46a353a20223b76b690294135f66b4a96849e",
+          "message": "Merge pull request #3456 from maqi/persist_merkle_uploaded_chunks_in_receipt_for_resume\n\nfix(client): persist merkle uploaded chunks in receipt for resume",
+          "timestamp": "2026-02-10T17:16:44Z",
+          "tree_id": "68972e92876e1cc5301bfc6bf867de6d15d6eb8c",
+          "url": "https://github.com/jacderida/autonomi/commit/b9b46a353a20223b76b690294135f66b4a96849e"
+        },
+        "date": 1770762424579,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 1.0515900201551092,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 5.5482662886164125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 6.588896425041827,
             "unit": "MiB/s"
           }
         ]
