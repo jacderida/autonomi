c77fed7c1fd23b8722e937a5f0a4c3fe75e7faf7
github-action-benchmark <github@users.noreply.github.com>
2026-02-17 16:23:03 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 92fc410518d465815066b005eb4fc2212dd65c5e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771012928794,
+  "lastUpdate": 1771345383291,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -1559,6 +1559,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "ant files download",
             "value": 6.591299882262838,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "92fc410518d465815066b005eb4fc2212dd65c5e",
+          "message": "Merge pull request #3459 from jacderida/feat-auto_upgrades_for_windows\n\nfeat(node): enable automatic upgrades on Windows",
+          "timestamp": "2026-02-17T15:58:57Z",
+          "tree_id": "bbc7568ddd3c391fe7ce8947c489beb92c6b1b80",
+          "url": "https://github.com/jacderida/autonomi/commit/92fc410518d465815066b005eb4fc2212dd65c5e"
+        },
+        "date": 1771345382376,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 1.048024356810866,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 5.4311125995888645,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 6.556676544583839,
             "unit": "MiB/s"
           }
         ]
