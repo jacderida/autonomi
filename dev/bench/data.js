3a7662a1a06fd626a9dc497f40187f9ea1f190fc
github-action-benchmark <github@users.noreply.github.com>
2026-02-24 23:00:28 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 3398478e907ff0394ff7862e55095c2c329fccb3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771974026651,
+  "lastUpdate": 1771974028023,
   "repoUrl": "https://github.com/jacderida/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -4355,6 +4355,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3398478e907ff0394ff7862e55095c2c329fccb3",
+          "message": "Merge pull request #3470 from maidsafe/dependabot/npm_and_yarn/ant-node-nodejs/tar-7.5.9\n\nchore(deps-dev): bump tar from 7.5.7 to 7.5.9 in /ant-node-nodejs",
+          "timestamp": "2026-02-23T20:12:47Z",
+          "tree_id": "d07b9649a4e844894ddf0866d645b3927a7f65cc",
+          "url": "https://github.com/jacderida/autonomi/commit/3398478e907ff0394ff7862e55095c2c329fccb3"
+        },
+        "date": 1771974027595,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 13,
             "unit": "MB"
           }
         ]
